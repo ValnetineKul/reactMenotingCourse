449f45606ceef1e4d74c79eec99e144549af4e49
vKul <valkulchikhin@gmail.com>
2021-09-28 09:00:50 +0300

    added propTypes

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -1,15 +1,26 @@
 import styles from './styles.module.scss';
 import { Text } from 'components';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { FC } from 'react';
 
-const Header = () => {
+type Props = {
+  className?: string,
+}
+
+const Header: FC<Props> = ({ className }) => {
   return (
-    <footer className={styles.footer}>
+    <footer className={cx(styles.footer, className)}>
       <div>
         <Text color="red" tag="span" weight="bold">netflix</Text>
         <Text color="red" tag="span">roulette</Text>
       </div>
     </footer>
   );
+};
+
+Header.propTypes = {
+  className: PropTypes.string
 };
 
 export default Header;
